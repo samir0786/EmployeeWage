8845f3ffd59d31fdedf38b25f675e0444b86fcaa
samir0786 <samirsiescom@gmail.com>
2020-04-16 21:06:07 +0300

    ADDED: Check Employee Attendence

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,2 +1,14 @@
-#!/bin/bash -x
-echo "Welcome to Employee Wage"
+public class EmployeeWage{
+
+	public static void main(String[] args){
+		//Constants
+		int IS_FULL_TIME =1;
+		//Computation
+		double empCheck= Math.floor(Math.random() * 10)% 2;
+		if( empCheck== IS_FULL_TIME){
+			System.out.println("Employee is Present");
+		}else{
+			System.out.println("Employee is Absent");
+		}
+	}
+}

0005a7de61db48d1609fef39046934f0fb120227
samir0786 <samirsiescom@gmail.com>
2020-04-17 11:07:48 +0300

    ADDED: Calculate Part Time Employee Wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -3,15 +3,18 @@ public class EmployeeWage
    public static void main(String[] args)
    {
       // Constants
-      int IS_FULL_TIME = 1;
+		int IS_FULL_TIME = 2;
+		int IS_PART_TIME = 1;
 		int EMP_RATE_PER_HOUR =20;
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
       // Computation
-      double empCheck = Math.floor(Math.random() * 10) % 2;
+      double empCheck = Math.floor(Math.random() * 10) % 3;
       if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
+		else if(empCheck ==IS_PART_TIME)
+			empHrs = 4;
 		else
 			empHrs = 0;
 		empWage = empHrs *  EMP_RATE_PER_HOUR;

3dd8bdc9ff947cf5148106cb7249a622cbb9ed91
samir0786 <samirsiescom@gmail.com>
2020-04-17 16:49:02 +0300

    ADDED: Calculate Employee Wage for Month

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,15 +1,16 @@
-public class EmployeeWage
-{
-		// Constants
-		static final int IS_FULL_TIME = 2;
-		static final int IS_PART_TIME = 1;
-		static final int EMP_RATE_PER_HOUR =20;
-		 public static void main(String[] args)
-   {
+public class EmployeeWage{
+	// Constants
+	static final int IS_FULL_TIME = 2;
+	static final int IS_PART_TIME = 1;
+	static final int EMP_RATE_PER_HOUR =20;
+	static final int WORKING_DAYS_PER_MONTH=20;
+	public static void main(String[] args){
 		// Variables
 		int empHrs = 0;
 		int empWage = 0;
+		int salary=0,total_salary=0;
 		// Computation
+		for(int day=1;day<=WORKING_DAYS_PER_MONTH;day++){
 		int empCheck = (int)(Math.random() * 10) % 3;
 		switch (empCheck){
 		case IS_FULL_TIME:
@@ -22,7 +23,9 @@ public class EmployeeWage
 			empHrs=0;
 			break;
 		}
-		empWage = empHrs *  EMP_RATE_PER_HOUR;
-		System.out.println("Emp Wage: " + empWage);
-   }
+		salary = empHrs *  EMP_RATE_PER_HOUR;
+		total_salary=salary+total_salary;
+	}
+		System.out.println("Emp Wage: " + total_salary);
+  }
 }
